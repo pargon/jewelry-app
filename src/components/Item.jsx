9644067a7f0e726034b698dcbae8856aab9ad7a2
Gonzalo Parra <gparra@VALKIMIA.COM>
2022-06-08 15:49:20 -0300

    add promises and map itemlist

---
+++ src/components/Item.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+function Item({ item }) {
+  return (
+    <>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={item.pictureUrl} />
+        <Card.Body>
+          <Card.Title>{item.title}</Card.Title>
+          <Card.Text>{item.description}</Card.Text>
+          <Card.Text>Price: {item.price}</Card.Text>
+          <Card.Link href="#" >Ver detalle del Producto</Card.Link>
+        </Card.Body>
+      </Card>
+    </>
+  );
+}
+
+export default Item;
