92158a053ff9f5016027c084f74d15b131574dd3
pargon <gonza.parra@gmail.com>
2022-06-13 22:15:58 -0300

    add events

---
+++ src/components/TestEvents.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const vocals = "aeiou";
+
+function Stackbliz() {
+  const controlVoc = (e) => {
+    const str = e.key.toLowerCase();
+    if(vocals.includes(str)){
+      alert('No puede ingresar vocales');
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <h1>Hola</h1>
+        <input onKeyDown={controlVoc} />
+      </div>
+    </>
+  );
+}
+
+export default Stackbliz;
